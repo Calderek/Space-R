3ef6f8ea425dd4ed7e5ba34df17bec135faf849f
Calderek <michal4421@outlook.com>
2017-07-25 22:39:14 +0200

    Dodanie playerhelpera, w  którym będą zapisane podstawowe stałe dane o użytkowniku

---
+++ SpaceR/Assets/Helper/PlayerHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Helper
+{
+    public static class PlayerHelper
+    {
+        // zmienna sterujacy czuloscia poruszania statkiem
+        public const float movSpeed = 5f; 
+    }
+}
--- SpaceR/Assets/Scripts/PlayerMovement.cs
+++ SpaceR/Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,18 @@
 ﻿//Skrypt ma za zadanie poruszanie statkiem gracza za pomoca stralek gora i dol oraz przyciskow w, s, a i d
 
 
+using Assets.Helper;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float movSpeed = 5f; // zmienna sterujacy czuloscia poruszania statkiem
-	
 	// Update is called once per frame
 	void Update () {
-        float h = Input.GetAxis("Horizontal") * Time.deltaTime * movSpeed;
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime * PlayerHelper.movSpeed;
             //przechowuje zawartość klikniecia zalezna od danej stralki(-1 do 1)
-        float v = Input.GetAxis("Vertical") * Time.deltaTime * movSpeed;
+        float v = Input.GetAxis("Vertical") * Time.deltaTime * PlayerHelper.movSpeed;
         //to samo tylko strzalki gora, dol
 
         transform.Translate(-v, h, 0);   //zmiana polozenia obiektu w oparciu o zawartosc zmiennych
