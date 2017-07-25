86bb5916bfd7f6e5fc13a10de5be59952b5b7275
Calderek <michal4421@outlook.com>
2017-07-26 01:12:31 +0200

    Dodanie klasy uruchamiającej  kod po uruchomieniu projektu, generowanie kilku cubów w pewnych odstepach

---
+++ SpaceR/Assets/Infrastructure/Startup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+/// <summary>
+/// Klasa wywołuje kod w statycznym konstruktorze dokładnie raz podczas uruchomienia projektu
+/// </summary>
+
+[InitializeOnLoad]
+public class Startup {
+
+    // Use this for initialization
+    static Startup()
+    {
+        EnemiesCreate creator = new EnemiesCreate();
+        creator.Create();
+    }
+}
---
+++ SpaceR/Assets/Scripts/EnemiesCreate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class EnemiesCreate {
+
+
+    public void Create()
+    {
+        //Material newMat = Resources.Load("Assets/Models/Materials/Material", typeof(Material)) as Material;
+        for(int i=0;i<16;i++)
+        {
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.name = "enemy " + (i + 1)  ;
+            cube.transform.position = new Vector3(16.98284F, 1.232025F, 8*i);
+            //GameObject prefab = AssetDatabase.LoadAssetAtPath("Assets/Models/spaceship", typeof(GameObject));
+            //GameObject clone = GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
+            //cube.GetComponent<Renderer>().material = newMat;
+            GameObject go = new GameObject("spaceship");
+            //go.name = "nazwa";
+        }
+
+    }
+     
+
+}
