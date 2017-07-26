333c0d689a956c3fa1aea99f107af1fbf388fb66
Calderek <michal4421@outlook.com>
2017-07-26 23:07:14 +0200

    Spawnowanie 16 wrogów w jednym miejscu w prawym górnym rogu planszy

---
+++ SpaceR/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public GameObject enemy_Perfabs;
+    public Transform place_spawn;
+
+	// Use this for initialization
+	void Start ()
+    {   
+        for(int i=0;i<16;i++)
+        {
+            var enemy=Instantiate(enemy_Perfabs, place_spawn);
+            enemy.name = "Enemy " + (i + 1);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
