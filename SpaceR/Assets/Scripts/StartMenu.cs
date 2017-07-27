da7cb9e1a8b946e3e262ad50a669a03714ad575a
JohnyMielony07 <jan.latoch@gmail.com>
2017-07-27 11:56:55 +0200

    Oprogramowanie startu

---
+++ SpaceR/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour {
+    
+    public Transform Player;
+    public GameObject ScreenStart;
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void GameActivation()
+    {
+        Player.GetComponent<PlayerMovement>().enabled = true;
+        ScreenStart.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
