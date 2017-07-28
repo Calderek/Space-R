4d2d2a625b9916b020adda1b85d3e501562d1724
JohnyMielony07 <jan.latoch@gmail.com>
2017-07-28 15:13:31 +0200

    Dodanie przemieszczania po menu za pomoca klawiatury

---
+++ SpaceR/Assets/AudioMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMenu : MonoBehaviour {
+    public GameObject AudioScreen;
+    public GameObject SettingsScreen;
+
+    public void BackFromAudio()
+    {
+        AudioScreen.SetActive(false);
+        SettingsScreen.SetActive(true);
+    }
+
+}
