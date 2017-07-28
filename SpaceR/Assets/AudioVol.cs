3cab5a49fcf03c55bcd4a3cf5ee9c47701d79253
JohnyMielony07 <jan.latoch@gmail.com>
2017-07-28 20:09:57 +0200

    Oprogramowanie ustawień audio

---
+++ SpaceR/Assets/AudioVol.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioVol : MonoBehaviour {
+
+    public AudioMixer masterMixer;
+
+    public void SetAudioLvl(float audioLvl)
+    {
+        masterMixer.SetFloat("masterVol", audioLvl);
+    }
+	
+}
