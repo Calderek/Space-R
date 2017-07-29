b147483d96c570865c519f8596b7b40e32560218
JohnyMielony07 <jan.latoch@gmail.com>
2017-07-29 21:03:19 +0200

    Dodanie panelu ustawien graficznych

---
+++ SpaceR/Assets/Scripts/Menu Scripts/GraphicsMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphicsMenu : MonoBehaviour {
+
+    public Toggle fullScreenToggle;
+    public Dropdown resolutionDropdown;
+    public Dropdown textureQualityDropdown;
+
+    public Resolution[] resolutions;
+    public GraphicsSettings graphicsSettings;
+
+    void OnEnable()
+    {
+        graphicsSettings = new GraphicsSettings();
+
+        fullScreenToggle.onValueChanged.AddListener(delegate { OnFullScreenToggle(); });
+        resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionChange(); });
+        textureQualityDropdown.onValueChanged.AddListener(delegate { OnTextureQualityChange(); });
+
+
+        resolutions = Screen.resolutions;
+    }
+
+    public void OnFullScreenToggle()
+    {
+        graphicsSettings.fullScreen = Screen.fullScreen = fullScreenToggle.isOn;
+        QualitySettings.masterTextureLimit = graphicsSettings.textureQuality = textureQualityDropdown.value; 
+    }
+
+    public void OnResolutionChange()
+    {
+
+    }
+
+    public void OnTextureQualityChange()
+    {
+
+    }
+    public void SaveSettings()
+    {
+
+    }
+
+}
