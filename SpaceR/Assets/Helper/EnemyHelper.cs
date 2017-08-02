77eb9e7774bfc9c0fc43e19a1775342c0aabf7e5
Davaeth <masterbelki@gmail.com>
2017-08-02 23:10:04 +0200

    test prędkości

--- SpaceR/Assets/Helper/EnemyHelper.cs
+++ SpaceR/Assets/Helper/EnemyHelper.cs
@@ -9,7 +9,7 @@ namespace Assets.Helper
     {
         public static int amount = 16;
         public static int defaultHealth = 5;
-        public static int defaultaVelocity = 40;
+        public static int defaultVelocity = 40;
 
     }
 }
