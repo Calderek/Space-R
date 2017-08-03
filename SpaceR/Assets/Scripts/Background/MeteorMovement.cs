273dc9d4906bd751828eeaa754d5de8084405593
Mateusz Drewniak <waldios1996@gmail.com>
2017-08-03 23:00:12 +0200

    O ciul. Meteory

---
+++ SpaceR/Assets/Scripts/Background/MeteorMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class MeteorMovement : MonoBehaviour
+{
+
+    private float meteorSpeed;
+    public float minMeteorSpeed = 5f;
+    public float maxMeteorSpeed = 20f;
+
+    private float meteorRotationX;
+    private float meteorRotationY;
+    private float meteorRotationZ;
+
+    public float minMeteorSize = 5f;
+    public float maxMeteorSize = 20f;
+
+    private float meteorSize;
+
+    private Vector3 position;
+
+    void Start ()
+    {
+    
+        meteorSize = Random.Range(minMeteorSize, maxMeteorSize);
+
+        //Jezeli rozmiar meteora jest duzy to bedzie poruszac sie szybko na 1 planie, mniejsze meteory beda poruszac sie w tle z mniejsza predkoscia (iluzja glebi)
+        if (meteorSize>13f)
+        {
+            meteorSpeed = Random.Range(minMeteorSpeed*2f, maxMeteorSpeed);
+            transform.position = new Vector3(transform.position.x, -10f, transform.position.z);
+        }
+        else
+        {
+            meteorSpeed = Random.Range(minMeteorSpeed, maxMeteorSpeed / 2f);
+            transform.position = new Vector3(transform.position.x, -30f, transform.position.z);
+        } 
+
+        transform.localScale = new Vector3(meteorSize, meteorSize, meteorSize);
+        meteorRotationX = Random.Range(25f, 50f);
+        meteorRotationY = Random.Range(25f, 50f);
+        meteorRotationZ = Random.Range(25f, 50f);
+        
+
+    }
+    void Update()
+    {
+        position = transform.position;
+        transform.Translate(0, 0, -1 * meteorSpeed*Time.deltaTime, Space.World);
+        transform.Rotate(meteorRotationX * Time.deltaTime, meteorRotationY * Time.deltaTime, meteorRotationZ * Time.deltaTime);
+        Destroy(gameObject, 50);
+    }
+   
+   
+}
