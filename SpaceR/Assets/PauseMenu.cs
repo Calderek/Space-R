b3a195f2225440d45296eabe83e14bebae37a6ee
JohnyMielony07 <jan.latoch@gmail.com>
2017-08-18 23:37:01 +0200

    Dodanie obsługi przycisków i esc

--- SpaceR/Assets/PauseMenu.cs
+++ SpaceR/Assets/PauseMenu.cs
@@ -5,8 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
 
+    public GameObject pauseScreen;
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            pauseScreen.SetActive(true);
+        }
+
+    }
+        
+
+
+
 	public void RestartGame()
     {
+        Time.timeScale = 1;
+        pauseScreen.SetActive(false);
         SceneManager.LoadScene(0);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pauseScreen.SetActive(false);
     }
 }
